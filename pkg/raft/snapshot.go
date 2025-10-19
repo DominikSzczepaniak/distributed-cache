d58b3db6af57462427c0fe88f08ad54a18f4d7f6
Dominik Szczepaniak <szczepaniakdominik5@gmail.com>
2025-10-19 13:52:28 +0200

    cleanup

--- pkg/raft/snapshot.go
+++ pkg/raft/snapshot.go
@@ -23,17 +23,11 @@ func newSnapshotter(cfg *Config) *Snapshot {
 	}
 }
 
-//TODO
-//on new snapshot do
-//1. delete all previous logs until lastIndex - TODO in some higher logic func - DONE in decideRunSnapshot()
-//2. delete all previous snapshots - DONE with O_CREATE on offset 0
-
 func (rds *DataSaver) openFileForWriting(path string, flags int) (*os.File, error) {
 	return os.OpenFile(path, flags, 0644)
 }
 
 func (rds *DataSaver) WriteSnapshotData(data []byte, offset int) (int, error) {
-	//TODO if new snapshot enters this function with offset = 0, abort all the currently running ones
 	var f *os.File
 	var err error
 
@@ -89,34 +83,7 @@ func (r *Raft) decideRunSnapshot() error {
 	r.snapshotter.lastIndex = r.commitedLength
 	r.snapshotter.lastTerm = lastTerm
 
-	// Do NOT change r.commitedLength and do NOT RestoreFromSnapshot locally.
 	return nil
-	//if len(r.log) >= r.snapshotter.snapshotThreshold {
-	//	slog.Info(fmt.Sprintf("We decided to run snapshot, because log length is %d, while threshold is %d. Time: %d:%d:%d", len(r.log), r.snapshotter.snapshotThreshold, time.Now().Minute(), time.Now().Second(), time.Now().Nanosecond()))
-	//	snpsht, err := r.application.GetSnapshot()
-	//	logLenInSnapshot := len(r.log)
-	//	lastTerm := r.log[len(r.log)-1].Term
-	//	slog.Info(fmt.Sprintf("Got data for snapshot. Length of logs: %d, Last term: %d", logLenInSnapshot, lastTerm))
-	//	if err != nil {
-	//		slog.Error(fmt.Sprintf("Getting application snapshot failed %s", err.Error()))
-	//		return err
-	//	}
-	//	_, err = r.logSaver.WriteSnapshotData(snpsht, 0)
-	//	if err != nil {
-	//		return err
-	//	}
-	//	slog.Info(fmt.Sprintf("Correctly wrote snapshot data. Cutting logs of length %d to %d - end", len(r.log), logLenInSnapshot-1))
-	//	newLastIndex := r.snapshotter.lastIndex + logLenInSnapshot
-	//
-	//	r.log = []LogEntry{}
-	//	r.snapshotter.lastIndex = newLastIndex
-	//	r.snapshotter.lastTerm = lastTerm
-	//	r.commitedLength = newLastIndex
-	//
-	//	err, key := r.application.RestoreFromSnapshot(snpsht)
-	//	slog.Info(fmt.Sprintf("When exited restore from snapshot, key value is %d", r.application.GetValue(key)))
-	//}
-	//return nil
 }
 
 func (rds *DataSaver) ReadSnapshotData() ([]byte, error) {
